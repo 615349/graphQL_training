124b1dc48ef6ce4080e2af9ef6975cae775d1d8a
Ben Li <ben.li@qantas.com.au>
2018-10-22 15:07:25 +1100

    remove one obsolte file

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -1,1 +1,14 @@
-export { default } from './Loading';
+import React from 'react';
+import './loading.scss';
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div className="loading__icon">
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  );
+}

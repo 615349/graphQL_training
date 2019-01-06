8502ddd5b5e18b91e68925d5059a37777acf1538
Ben Li <ben.li@qantas.com.au>
2019-01-07 10:08:40 +1100

    remove some unreadable code

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
-import AppContainer from './containers/';
 import Routes from './routes';
+import { ApolloProvider } from 'react-apollo';
 import './styles/main.scss';
 
 const client = new ApolloClient({ uri: 'http://localhost:3001/graphql' });
 
 ReactDOM.render(
-  <AppContainer apolloClient={client}>
-    <Routes />
-  </AppContainer>,
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>,
   document.getElementById('root'),
 );
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import CoreLayout from '../layouts/CoreLayout';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import Home from '../components/Home';
 import FAQs from '../components/FAQs';
 import NotFound from '../components/NotFound';
+import Navigation from '../components/Navigation';
+
+function transformPathToClass(pathname) {
+  return pathname.replace('/', '')
+}
 
 export default function createRoutes() {
+  const { pathname } = window.location;
   return (
-    <CoreLayout>
-      <Switch>
-        <Route component={Home} exact path="/" />
-        <Route component={FAQs} path="/faqs" />
-        <Route component={NotFound} />
-      </Switch>
-    </CoreLayout>
+    <BrowserRouter>
+      <main className={`page-${transformPathToClass(pathname)}`}>
+        <Navigation />
+        <Switch>
+          <Route component={Home} exact path="/" />
+          <Route component={FAQs} path="/faqs" />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
